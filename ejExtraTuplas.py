a9c1669016e01df81ca872279fc76d85489275b8
Paula G <gonzaleztoscano.pau@gmail.com>
2023-05-23 11:06:10 -0300

    Ejercicios extra 1

---
+++ ejExtraTuplas.py
@@ -0,0 +1,25 @@
+#Escribe un programa que solicite al usuario que ingrese una lista de números enteros.
+#El programa debe crear una tupla a partir de la lista y luego imprimir la tupla en orden
+#inverso.
+msj=""
+num=0
+c=0
+listaNum=[]
+listaNumRev=[]
+msj="Ingresa 10 diferentes numeros para generar una lista"
+print(msj)
+while c!=10:
+    num=(input())
+    listaNum.append(num)
+    c+=1
+print(f"La lista generada es: {listaNum}")
+
+for num in reversed(listaNum):
+    print(num)
+    listaNumRev.append(num)
+
+tuplListaNum=tuple(listaNumRev)
+
+print(f"la tupla generada reversa es: {tuplListaNum}")
+
+
