6488b1be0905401e701692f7e4634eaf317b8718
Paula G <gonzaleztoscano.pau@gmail.com>
2023-05-23 11:24:57 -0300

    Validación

--- ejExtraTuplas.py
+++ ejExtraTuplas.py
@@ -6,19 +6,21 @@ num=0
 c=0
 listaNum=[]
 listaNumRev=[]
+
 msj="Ingresa 10 diferentes numeros para generar una lista"
 print(msj)
 while c!=10:
     num=(input())
-    listaNum.append(num)
-    c+=1
-print(f"La lista generada es: {listaNum}")
+    if num.isnumeric()==True:
+        listaNum.append(num)
+        c+=1
+    else:
+        msj="El programa solo acepta numeros enteros, por favor vuelve a iniciarlo"
+        print(msj)
+    break
 
+print(f"La lista generada es: {listaNum}")
 for num in reversed(listaNum):
     listaNumRev.append(num)
-
 tuplListaNum=tuple(listaNumRev)
-
 print(f"la tupla generada reversa es: {tuplListaNum}")
-
-
